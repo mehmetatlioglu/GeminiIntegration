c522abe90580d5f637db64cfc57da5892ca20cc2
Mehmet Niyazi Atlıoğlu <70020093+mehmetatlioglu@users.noreply.github.com>
2024-02-14 14:24:19 +0300

    photo loading and content explaining done

--- app/src/main/java/com/example/geminiintegration/MainActivity.kt
+++ app/src/main/java/com/example/geminiintegration/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.geminiintegration
 
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.viewModelFactory
+import coil.compose.rememberAsyncImagePainter
 import com.example.geminiintegration.ui.theme.GeminiIntegrationTheme
+import com.google.ai.client.generativeai.GenerativeModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +40,7 @@ class MainActivity : ComponentActivity() {
             GeminiIntegrationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -39,5 +60,66 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     GeminiIntegrationTheme {
         Greeting("Android")
+    }
+}
+
+
+@Composable
+fun MainScreen(viewModel: MainViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+    val context = LocalContext.current
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri ->
+            uri?.let {
+                val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, it))
+                } else {
+                    MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+                }
+                viewModel.setSelectedImageBitmapData(bitmap)
+                viewModel.onUriSelection(it)
+            }
+        }
+    )
+    Column(verticalArrangement = Arrangement.SpaceBetween) {
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(48.dp)) {
+            viewModel.selectedImageUri.value?.let {
+                Image(
+                    painter = rememberAsyncImagePainter(model = it),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 24.dp),
+                    contentScale = ContentScale.Crop
+                )
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = viewModel.suggestionText.value,
+                    onValueChange = viewModel::onSuggestionTextChanged
+                )
+                TextButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = viewModel::onSuggestionClicked
+                ) {
+                    Text(text = "Suggestion")
+                }
+
+                Column(
+                    modifier = Modifier.verticalScroll(rememberScrollState())
+                ) {
+                    Text(text = viewModel.contentText.value)
+                }
+
+            }
+        }
+
+        TextButton(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { galleryLauncher.launch("image/*") }
+        ) {
+            Text(text = "Pick image")
+        }
     }
 }
